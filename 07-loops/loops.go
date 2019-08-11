48b04d53aa348f67775dfdab4c73827857d32aad
David Poulos <davidstephenpoulos@gmail.com>
2019-08-11 14:59:03 -0500

    Learning loops and range

---
+++ 07-loops/loops.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main() {
+	characters := make([]string, 10)
+
+	characters = append(characters, "Harry Potter", "Hermione Granger", "Ronald Weasley")
+
+	i := 0
+
+	// Standard Iteration (While loop)
+	for i < len(characters) {
+		if len(characters[i]) == 0 {
+			i++
+			continue
+		}
+
+		fmt.Println("Character => " + characters[i])
+		i++
+	}
+
+	// Infinite Loops
+	for {
+		break // Break so I don't crash
+	}
+
+	// For loop
+	for i := 0; i < len(characters); i++ {
+
+	}
+
+	// Using RANGE
+	for _, person := range characters {
+		fmt.Println("Character => " + person)
+	}
+
+	cities := map[int]string{
+		0: "Austin",
+		1: "Dallas",
+	}
+
+	for key, city := range cities {
+		fmt.Printf("Key => %d City => %s\n", key, city)
+	}
+
+}
